b3e64153b4a9bef5e4c421990fc8952933871355
V5799 (Course ID) <v5799@cam.ac.uk>
2015-08-14 11:18:12 +0100

    .

--- drone-backend.js
+++ drone-backend.js
@@ -1,5 +1,33 @@
 var Cylon = require('cylon');
 var bot;
+function fly (robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    bot.drone.left(0.2);
+    after (2*1000, function () {
+        bot.drone.left(0);
+    });
+    bot.drone.forward(0.2);
+    after (4*1000, function (){
+        bot.drone.forward(0)
+    });
+
+    bot.drone.right(0.2);
+    after(6*1000, function(){
+    bot.drone.right(0)
+});
+
+
+    after (10*1000, function (){
+    bot.drone.land();
+
+});
+    after (12*1000, function (){
+        bot.drone.stop ();
+    });
+}
 
 // Initialise the robot
 Cylon.robot()
